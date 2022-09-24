5d0bf0bb114f9e681830aff906c9777cc8b9ba3f
vanelozano7312 <vane.lozano7312@gmail.com>
2022-09-24 16:49:07 -0500

    upload file and different views

--- mazeprocess.py
+++ mazeprocess.py
@@ -1,8 +1,19 @@
 import csv, pygame
 from colors import colors
+import shutil
+import tkinter, os
+import tkinter.filedialog
+
+#Función que permite al usuario subir un archivo .csv y lo guarda en static/mazes/user_maze
+def upload_maze():
+    top = tkinter.Tk()
+    top.withdraw()  
+    file_name = tkinter.filedialog.askopenfilename(parent=top, title = "Select file",filetypes = (("csv files","*.csv"), ))
+    shutil.copy(file_name, "./static/mazes/user_maze.csv")
+    top.destroy()
 
 #Función que dado un path de un archivo .csv retorna el tamaño (filas, columnas) del laberinto 
-def mazesize(path):
+def maze_size(path):
     with open(path) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
@@ -17,8 +28,8 @@ def mazesize(path):
 
 
 #Función que dada una matriz MxN calcula el tamaño de cada celda y el inicio en X y Y a dibujar
-# (está como si la pantalla default fuera de 700x700)
-def displaysize(m, n):
+# (está como si la pantalla de juego default fuera de 700x700)
+def display_size(m, n):
     if m>=n:
         cell_size = 700/m
         x_start = (700 - (n*cell_size))/2
@@ -32,7 +43,7 @@ def displaysize(m, n):
     
 #Función que dibuja el maze
 # (está como si la pantalla default fuera de 700x700)
-def mazedraw(screen, color, path, cell_size, x_start, y_start):
+def maze_draw(screen, color, path, cell_size, x_start, y_start):
     pygame.draw.rect(screen, colors[color][1], (0, 0, 700, 700))
     with open(path) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
--- mazesolver.py
+++ mazesolver.py
@@ -1,101 +1,82 @@
 import pygame, sys, random
 from mazeprocess import *
 from colors import colors
-pygame.init()
-pygame.display.set_caption('Maze solver')
 
 size = (1000, 700)
-path = 'static/maze_50x50.csv'
-m, n = mazesize(path)          #tamaño mxn del maze
-cell_size, x_start, y_start = displaysize(m, n)
-color = random.choice(list(colors.keys()))
-
-# #crear ventana
-# screen = pygame.display.set_mode(size)
-# #fps
-# clock =  pygame.time.Clock()
-# #cargar imagenes y escalarlas
-# maze_view = pygame.image.load('static/images/maze_view.png')
-# maze_view = pygame.transform.scale(maze_view, size)
-# menu = pygame.image.load('static/images/menu.png')
-# menu = pygame.transform.scale(menu, size)
-# a = pygame.image.load('static/images/resolverpor/a.png')
-
-
-
-# while True:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             sys.exit()
-            
-#     screen.blit(maze_view, [0, 0])
-    
-#     #fondo
-#     mazedraw(screen, color, path, cell_size, x_start, y_start)
-    
-#     # screen.blit(a, [0, 0])
-#     screen.blit(menu, [0, 0])
-    
-#     #mouse
-#     mouse_pos = pygame.mouse.get_pos()
-#     #zona de dibujo
-    
-#     #actualizar pantalla
-#     pygame.display.flip()
-#     clock.tick(60)
-    
-    
-# pygame.quit()
 
 
 class Game(object):
     
     def __init__(self):
-        self.game_over = False
-
+        self.solved_maze = False
+        self.page = 'home'              #posibles páginas: home, maze
+        self.solve_method =''           #posibles methods: a:anchura, ae:a*, bdcu:búsqeda de costo uniforme, bg:búsqueda greedy
+                                        #p: profundidad, pi:profundidad iterativa, '': no se ha escogido
+        self.path = 'static/mazes/maze_50x50.csv'
+        self.m, self.n = maze_size(self.path)          #tamaño mxn del maze
+        self.cell_size, self.x_start, self.y_start = display_size(self.m, self.n)
+        self.color = random.choice(list(colors.keys()))
 
     def process_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return True
 
-			# if event.type == pygame.MOUSEBUTTONDOWN:
-			# 	if self.game_over:
-			# 		self.__init__()
-
+            #Si ya acabó de resolver el laberinto un click en la pantalla lo devuelve al home page
+            if self.solved_maze:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.__init__()
         return False
 
     def run_logic(self):
 
-        if not self.game_over:
-            aNN=0
+        if not self.solved_maze:
+            if self.page == 'home':
+                pass
+            elif self.page == 'maze':
+                pass
 
     def display_frame(self, screen):
 
-        if self.game_over:
-			# font = pygame.font.SysFont("serif", 25) # Fuente
-			# text = font.render("Game Over, Click To Continue", True, BLACK) # Texto
-			# center_x = (SCREEN_WIDTH // 2) - (text.get_width() // 2) # posicion text
-			# center_y = (SCREEN_HEIGHT // 2) - (text.get_height() // 2)
-			# screen.blit(text, [center_x, center_y]) # ponerlo en pantalla
-            aNN = 1
+        if self.solved_maze:
+            font = pygame.font.SysFont("serif", 25) # Fuente
+            text = font.render("Game Over, Click To Continue", True, (0, 0, 0)) # Texto
+            center_x = (size[0] // 2) - (text.get_width() // 2) # posicion text
+            center_y = (size[1] // 2) - (text.get_height() // 2)
+            screen.blit(text, [center_x, center_y]) # ponerlo en pantalla
 
-        if not self.game_over:   
-            #cargar imagenes y escalarlas
-            maze_view = pygame.image.load('static/images/maze_view.png')
-            maze_view = pygame.transform.scale(maze_view, size)
-            menu = pygame.image.load('static/images/menu.png')
-            menu = pygame.transform.scale(menu, size)
-            a = pygame.image.load('static/images/resolverpor/a.png')
-            screen.blit(maze_view, [0, 0])
+        if not self.solved_maze:   
+            if self.page == 'home':
+                #cargar imagenes y escalarlas
+                home_view = pygame.image.load('static/images/home_view.png')
+                home_view = pygame.transform.scale(home_view, size)
+                screen.blit(home_view, [0, 0])
+                
+            elif self.page == 'maze':
+            ##Mostramos el maze
+                #cargar imagenes y escalarlas
+                maze_view = pygame.image.load('static/images/maze_view.png')
+                maze_view = pygame.transform.scale(maze_view, size)
+                screen.blit(maze_view, [0, 0])
+                
+                #fondo
+                maze_draw(screen, self.color, self.path, self.cell_size, self.x_start, self.y_start)
+                
+            ##Mostramos el menú si aún no se escogío un método
+                if self.solve_method == '':
+                    #cargar imagenes y escalarlas
+                    menu = pygame.image.load('static/images/menu.png')
+                    menu = pygame.transform.scale(menu, size)
+                    screen.blit(menu, [0, 0])
+                    
+            ##Si no mostramos el método escogido
+                else:
+                    selected_method = pygame.image.load(f"static/images/resolverpor/{self.solve_method}.png")
+                    selected_method = pygame.transform.scale(selected_method, (245, 130))
+                    screen.blit(selected_method, [740, 156])
             
-            #fondo
-            mazedraw(screen, color, path, cell_size, x_start, y_start)
-            
-            # screen.blit(a, [0, 0])
-            screen.blit(menu, [0, 0])
-
         pygame.display.flip()
+
 
 def main():
     pygame.init()
@@ -106,10 +87,7 @@ def main():
     #fps
     clock =  pygame.time.Clock()
     
-
     done = False
-    clock = pygame.time.Clock()
-
     game = Game()
 
     while not done:
